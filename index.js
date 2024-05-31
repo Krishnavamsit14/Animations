228884e16ed741336db668d5967ea26c73c15749
Krishnavamsit14 <krishnavamsit14@gmail.com>
2024-05-31 20:25:29 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var typed6 = new Typed('#mytext', {
+    strings: ['JavaScript Animations'],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true,
+    showCursor: 0
+  });
+  AOS.init({
+    duration: 1500
+  })
